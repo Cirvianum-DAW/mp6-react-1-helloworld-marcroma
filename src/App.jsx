ae35ad012bd3f93306fe86aceaf86d7d5444f12d
Marc Roma <marc.roma@cirvianum.cat>
2024-04-17 17:05:40 +0200

    Sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,75 @@
 import React from "react";
 import { useState } from "react";
 import Form from "./components/Form";
+import StudentList from "./components/StudentList";
 
-function App() {
-  // Afegim la variable d'estat tipusEstudiant
-  const [tipusEstudiant, setTipusEstudiant] = useState("no-graduat");
+const App = () => {
+  const [tipusEstudiant, setTipusEstudiant] = useState("No Graduat");
+  const [ngPlaces, setNGPlaces] = useState(60);
+  const [gPlaces, setGPlaces] = useState(40);
 
-  // Afegim la funció que actualitza el valor de la variable d'estat tipusEstudiant
   const handleChange = (e) => {
     setTipusEstudiant(e.target.value);
   };
 
-  // const [places, setPlaces] = useState(100);
-  const [ngPlaces, setPlacesNoGraduats] = useState(60);
-  const [gPlaces, setPlacesGraduats] = useState(40);
-
   const setPlacesDisponibles = (updatedPlaces) => {
-    tipusEstudiant === "no-graduat"
-      ? setPlacesNoGraduats(updatedPlaces)
-      : setPlacesGraduats(updatedPlaces);
+    tipusEstudiant === "Graduat"
+      ? setGPlaces(updatedPlaces)
+      : setNGPlaces(updatedPlaces);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-5 h-screen">
-      <div className="tipusEstudiant">
-        <label className="text-2xl text-center mx-2 block">
-          Places Disponibles No Graduats: {ngPlaces} <br/>
-          Places Disponibles Graduats: {gPlaces}
-        </label>
-        <label className="mr-2">Selecciona Tipus d'Estudiant:</label>
-        {/* Afegim el desplegable que permet triar d'acord amb les variables i
-        que crida la funció handleChange */}
-        <select
-          className="appDropDown border rounded-md py-1 px-2"
-          onChange={handleChange}
-          value={tipusEstudiant}
-        >
-          <option value="no-graduat">No Graduat</option>
-          <option value="graduat">Graduat</option>
-        </select>
+    <div className="App flex h-screen flex-col items-center justify-center ">
+      <div className="programes my-2">
+        <h3 className="title my-2 text-2xl text-blue-500">
+          Formulari d'inscripció d'estudiants.
+        </h3>
+        <ul className="ulInscripcio ">
+          <li
+            className="parentLabels my-2 flex items-center justify-evenly"
+            onChange={handleChange}
+          >
+            <label className="radioLabel">
+              <input
+                type="radio"
+                value="No Graduat"
+                name="programGroup"
+                defaultChecked
+                className="radioInput mr-2"
+              />
+              No Graduat
+            </label>
+            <label className="radioLabel">
+              <input
+                type="radio"
+                value="Graduat"
+                name="programGroup"
+                className="radioInput mr-2"
+              />
+              Graduat
+            </label>
+          </li>
+          <li className="parentLabels my-2">
+            Places disponibles per estudiant{" "}
+            <strong>
+              {tipusEstudiant}:{" "}
+              {tipusEstudiant === "Graduat" ? gPlaces : ngPlaces}
+            </strong>
+          </li>
+        </ul>
       </div>
       <Form
         tipusEstudiantSelect={tipusEstudiant}
         setPlacesDisponibles={setPlacesDisponibles}
-        placesActuals={tipusEstudiant === "no-graduat" ? ngPlaces : gPlaces}
+        placesActuals={tipusEstudiant === "Graduat" ? gPlaces : ngPlaces}
+      />
+      
+      <StudentList
+        detallsEstudiant={detallsEstudiant}
+        setDetallsEstudiant={setDetallsEstudiant}
       />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,51 +1,92 @@
 import React, { useState } from "react";
 
-function Form(props) {
+const Form = (props) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   const [welcomeMessage, setWelcomeMessage] = useState("");
 
-  const handleSubmit = (e) => {
-    setWelcomeMessage(`Hola ${firstName} ${lastName}!`);
+  const handleClick = (event) => {
+    handleInputReset('', '', '');
     props.setPlacesDisponibles(props.placesActuals - 1);
+    // Generació d'un ID per l'estudiant - 4digit
+    const randomKey = Math.floor(1000 + Math.random() * 9000);
+    let id = randomKey;
+    props.setDetallsEstudiant({
+      key: id,
+      fname: firstName,
+      lname: lastName,
+      programa: props.tipusEstudiantSelect,
+      email: email,
+    });
+    event.preventDefault(); // Necessari per evitar que el form es refresqui
+  };
 
-    e.preventDefault();
+  //change of input value set method
+  const handleInputChange = (setInput, event) => {
+    setInput(event.target.value);
+  };
+
+  //set input fields
+  const handleInputReset = (firstName, lastName, email) => {
+    setFirstName(firstName);
+    setLastName(lastName);
+    setEmail(email);
   };
 
   return (
-    <div className="m-7 w-80">
-      <form className="p-4" onSubmit={handleSubmit}>
-        <h1 className="text-3xl font-bold text-center mb-8">
-          Detalls d'estudiant: {props.tipusEstudiantSelect}
-        </h1>
-        <label className="block">Nom:</label>
-        <input
-          className="w-full mb-4 p-2 rounded-lg bg-gray-200"
-          type="text"
-          name="fname"
-          onBlur={(e) => setFirstName(e.target.value)}
-        />{" "}
-        <br />
-        <label className="pt-2 block">Cognom:</label>
-        <input
-          className="w-full p-2 rounded-lg bg-gray-200"
-          type="text"
-          name="lname"
-          onBlur={(e) => setLastName(e.target.value)}
-        />
-        <br />
-        <br />
-        <input
-          type="submit"
-          value="Submit"
-          className="p-1 bg-green-600 rounded font-bold text-white w-full"
-        />
+    <div className="flex h-screen justify-center">
+      <form className="enrolForm w-80" name="enrolForm">
+        <ul className="ulEnrol">
+          <li>
+            <input
+              className="border-1 mb-4 w-full rounded-lg border-dotted border-black bg-gray-200 p-2"
+              type="text"
+              name="firstname"
+              placeholder="Nom"
+              value={firstName}
+              onChange={(event) => handleInputChange(setFirstName, event)}
+            />
+          </li>
+          <li>
+            <input
+              className="border-1 mb-4 w-full rounded-lg border-dotted border-black bg-gray-200 p-2"
+              type="text"
+              name="lastname"
+              placeholder="Cognom"
+              value={lastName}
+              onChange={(event) => handleInputChange(setLastName, event)}
+            />
+          </li>
+          <li>
+            <input
+              className="border-1 mb-4 w-full rounded-lg border-dotted border-black bg-gray-200 p-2"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => handleInputChange(setEmail, event)}
+            />
+          </li>
+          <li>
+            <input
+              className="mb-4 w-full rounded bg-blue-500 p-2 px-4 py-2 font-bold text-white hover:bg-blue-700"
+              type="submit"
+              name="Enrol"
+              alt="Enrol"
+              value="Inscripció"
+              onClick={handleClick}
+            />
+          </li>
+          <li>
+            <label className="mb-4 block w-full p-2 text-2xl" id="studentMsg">
+              {welcomeMessage}
+            </label>
+          </li>
+        </ul>
       </form>
-
-      <label className="block w-full text-4xl mb-4 p-2" id="studentMsg">
-        {welcomeMessage}
-      </label>
     </div>
   );
-}
+};
+
 export default Form;
